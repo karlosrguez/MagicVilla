d6eb8c927b88ddd914a1f54a1dc11cd9ffba9339
karlosrguez <rguez.karlos@gmail.com>
2023-09-07 11:25:21 -0600

    DbContext - inyeccion de Dependencias

---
+++ MagicVilla_API/Datos/AplicarionDbContext.cs
@@ -0,0 +1,60 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace MagicVilla_API.Datos
+{
+    public class AplicarionDbContext : DbContext
+    {
+        public AplicarionDbContext(DbContextOptions<AplicarionDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configura la cadena de conexión y habilita el retry en caso de fallos transitorios.
+            optionsBuilder.UseSqlServer(
+                "Server=192.15.200.45;Database=MagicVilla;TrustServerCertificate=true;User Id=Prueba;Password=serversql;MultipleActiveResultSets=true",
+                options => options.EnableRetryOnFailure());
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Villa> Villas { get; set; } 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Villa>().HasData(
+            new Villa()
+            {
+                Id = 1,
+                Nombre = "Villa Real",
+                detalle = "Detalle de la Villa",
+                ImagenUrl = "",
+                Ocupantes = 5,
+                MetrosCuadrados = 50,
+                tarifa = 200,
+                Amenidad = "",
+                FechaCreacion = DateTime.Now,
+                FechaActualizacion = DateTime.Now,
+            },
+
+              new Villa()
+              {
+                  Id = 2,
+                  Nombre = "Premium Vista a la piscina",
+                  detalle = "Detalle de la Villa",
+                  ImagenUrl = "",
+                  Ocupantes = 4,
+                  MetrosCuadrados = 40,
+                  tarifa = 150,
+                  Amenidad = "",
+                  FechaCreacion = DateTime.Now,
+                  FechaActualizacion = DateTime.Now,
+              }
+              );
+    }
+
+    }
+}
---
+++ MagicVilla_API/Migrations/20230907154250_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "detalle", "tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", new DateTime(2023, 9, 7, 9, 42, 50, 9, DateTimeKind.Local).AddTicks(4292), new DateTime(2023, 9, 7, 9, 42, 50, 9, DateTimeKind.Local).AddTicks(4279), "", 50, "Villa Real", 5, "Detalle de la Villa", 200.0 },
+                    { 2, "", new DateTime(2023, 9, 7, 9, 42, 50, 9, DateTimeKind.Local).AddTicks(4295), new DateTime(2023, 9, 7, 9, 42, 50, 9, DateTimeKind.Local).AddTicks(4295), "", 40, "Premium Vista a la piscina", 4, "Detalle de la Villa", 150.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
